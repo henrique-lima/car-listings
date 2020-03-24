bf2c5e6646d95989eebb24adb9253a39dfbe1c63
henrique-lima <henrique.rodriguescruz@yahoo.com>
2020-03-24 18:40:19 +0000

    first commit

---
+++ src/main/java/com/hey/car/carlistings/CarListingsApplication.java
@@ -0,0 +1,13 @@
+package com.hey.car.carlistings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarListingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarListingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hey/car/carlistings/CarListingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hey.car.carlistings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarListingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
